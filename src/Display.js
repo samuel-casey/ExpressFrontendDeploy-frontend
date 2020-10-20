33e2bb8f939d6054ab44084dbb80d759fccfe14c
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-19 23:43:19 -0400

    creating new repo for frontend

--- src/Display.js
+++ src/Display.js
@@ -11,6 +11,7 @@ const Display = (props) => {
 					<img src={dog.img} alt={dog.name} />
 					<h1>{dog.name}</h1>
 					<h3>Age: {dog.age}</h3>
+					<p>{dog._id}</p>
 					<button
 						className='edit'
 						onClick={() => {
